e13b6b3296b0ead797edc1e406f9b9c1563a8a84
Kasun <kasun.w@duosoftware.com>
2017-04-27 15:04:36 +0530

    Version 6.1.0.7

--- settings/dialogs/webhooks/add-webhook-dialog.controller.js
+++ settings/dialogs/webhooks/add-webhook-dialog.controller.js
@@ -1,155 +1,170 @@
 (function ()
 {
-  'use strict';
-  angular
-    .module('app.settings')
-    .controller('AddWebhookController', addWebhookController);
+	'use strict';
+	angular
+		.module('app.settings')
+		.controller('AddWebhookController', addWebhookController);
 
-  /** @ngInject */
-  function addWebhookController(notifications,$charge,$mdDialog, $scope,webHooks,webhook,webhookEventList,webhookTypeChange,webhookSubmitted,enabledEditWH,resetWebhook,tempUsedEventsList,skipAllWebhooks,loadWebhookListPaging)
-  {
-    var vm=this;
+	/** @ngInject */
+	function addWebhookController(notifications,$charge,$mdDialog, $scope,webHooks,webhook,webhookEventList,webhookTypeChange,webhookSubmitted,enabledEditWH,tempUsedEventsList,skipAllWebhooks,loadWebhookListPaging)
+	{
+		var vm=this;
 
-	  // $scope.submitWebhook = submitWebhook;
-      $scope.webhookTypeChange = webhookTypeChange;
-      vm.webhookSubmitted = webhookSubmitted;
-      vm.webHooks = webHooks;
-      vm.webhookEventList = webhookEventList;
-	  $scope.webhook = webhook;
-	  $scope.enabledEditWH = enabledEditWH;
-      $scope.resetWebhook = resetWebhook;
-      $scope.skipAllWebhooks = skipAllWebhooks;
-      $scope.tempUsedEventsList = tempUsedEventsList;
-      $scope.loadWebhookListPaging = loadWebhookListPaging;
-      $scope.tempUsedEventsList = tempUsedEventsList;
+		// $scope.submitWebhook = submitWebhook;
+		$scope.webhookTypeChange = webhookTypeChange;
+		vm.webhookSubmitted = webhookSubmitted;
+		vm.webHooks = webHooks;
+		vm.webhookEventList = webhookEventList;
+		$scope.webhook = webhook;
+		$scope.enabledEditWH = enabledEditWH;
+		// $scope.resetWebhook = resetWebhook;
+		$scope.skipAllWebhooks = skipAllWebhooks;
+		$scope.tempUsedEventsList = tempUsedEventsList;
+		$scope.loadWebhookListPaging = loadWebhookListPaging;
+		$scope.tempUsedEventsList = tempUsedEventsList;
 
-      $scope.closeDialog = function () {
-		  $scope.webhook={};
-		  $scope.webhook.type="custom";
-		  $scope.webhook.mode="Live";
-		  $scope.webhookTypeChange("custom");
+		$scope.closeDialog = function () {
+			$scope.webhook={};
+			$scope.webhook.type="custom";
+			$scope.webhook.mode="Live";
+			$scope.webhookTypeChange("custom");
 
-		  vm.webhookList=[];
-		  skipAllWebhooks=0;
-		  tempUsedEventsList=[];
-          $mdDialog.hide();
-      }
+			vm.webhookList=[];
+			skipAllWebhooks=0;
+			tempUsedEventsList=[];
+			$scope.resetWebhook();
+			$mdDialog.hide();
+		};
 
-	  $scope.submitWebhook= function (editing) {
-		  if(!editing)
-		  {
-			  if (vm.webHooks.$valid == true) {
-				  vm.webhookSubmitted = true;
+		$scope.resetWebhook= function () {
+			$scope.webhook={};
+			$scope.webhook.type="custom";
+			$scope.webhook.mode="Live";
+			$scope.webhookTypeChange("custom");
 
-				  var webhookObj={};
-				  var tempEventsSelected=false;
-				  webhookObj.endPoint=$scope.webhook.endPoint;
-				  webhookObj.type=$scope.webhook.type;
-				  webhookObj.createdDate=new Date();
-				  webhookObj.isEnabled=true;
-				  webhookObj.eventCodes=[];
+			vm.webhookList=[];
+			skipAllWebhooks=0;
+			tempUsedEventsList=[];
+			$scope.loadWebhookListPaging();
 
-				  for (var i = 0; i < vm.webhookEventList.length; i++) {
-					  if(vm.webhookEventList[i].isSelected)
-					  {
-						  webhookObj.eventCodes.push(vm.webhookEventList[i].eventType);
-						  tempEventsSelected=true;
-					  }
-				  }
+			$scope.enabledEditWH=false;
+		};
 
-				  if(tempEventsSelected)
-				  {
-					  $charge.webhook().createWH(webhookObj).success(function (data) {
-						  console.log(data);
-						  // debugger;
-						  if(data.error=="00000")
-						  {
-							  notifications.toast("Webhook Created Successfully", "success");
-							  $scope.webhook={};
-							  $scope.webhook.type="custom";
-							  $scope.webhook.mode="Live";
-							  $scope.webhookTypeChange("custom");
+		$scope.submitWebhook= function (editing) {
+			if(!editing)
+			{
+				if (vm.webHooks.$valid == true) {
+					vm.webhookSubmitted = true;
 
-							  vm.webhookList=[];
-							  skipAllWebhooks=0;
-							  tempUsedEventsList=[];
-							  $mdDialog.hide();
-							  $scope.loadWebhookListPaging();
-						  }
-						  else
-						  {
-							  notifications.toast("Webhook Creation Failed", "error");
-						  }
-						  //$scope.webhook={};
-						  vm.webhookSubmitted = false;
-					  }).error(function (data) {
-						  $mdDialog.hide();
-						  console.log(data);
-						  vm.webhookSubmitted = false;
-					  })
-				  }
-				  else
-				  {
-					  notifications.toast("Select Events for the Webhook", "error");
-					  vm.webhookSubmitted = false;
-				  }
+					var webhookObj={};
+					var tempEventsSelected=false;
+					webhookObj.endPoint=$scope.webhook.endPoint;
+					webhookObj.type=$scope.webhook.type;
+					webhookObj.createdDate=new Date();
+					webhookObj.isEnabled=true;
+					webhookObj.eventCodes=[];
 
-			  }
-		  }
-		  else
-		  {
-			  if (vm.webHooks.$valid == true) {
-				  vm.webhookSubmitted = true;
+					for (var i = 0; i < vm.webhookEventList.length; i++) {
+						if(vm.webhookEventList[i].isSelected)
+						{
+							webhookObj.eventCodes.push(vm.webhookEventList[i].eventType);
+							tempEventsSelected=true;
+						}
+					}
 
-				  var webhookObj={};
-				  var tempEventsSelected=false;
-				  webhookObj.guWebhookId=$scope.webhook.guWebhookId;
-				  webhookObj.endPoint=$scope.webhook.endPoint;
-				  webhookObj.type=$scope.webhook.type;
-				  webhookObj.createdDate=new Date();
-				  webhookObj.isEnabled=true;
-				  webhookObj.eventCodes=[];
+					if(tempEventsSelected)
+					{
+						$charge.webhook().createWH(webhookObj).success(function (data) {
+							console.log(data);
+							// debugger;
+							if(data.error=="00000")
+							{
+								notifications.toast("Webhook Created Successfully", "success");
+								$scope.webhook={};
+								$scope.webhook.type="custom";
+								$scope.webhook.mode="Live";
+								$scope.webhookTypeChange("custom");
 
-				  for (var i = 0; i < vm.webhookEventList.length; i++) {
-					  if(vm.webhookEventList[i].isSelected)
-					  {
-						  webhookObj.eventCodes.push(vm.webhookEventList[i].eventType);
-						  tempEventsSelected=true;
-					  }
-				  }
+								vm.webhookList=[];
+								skipAllWebhooks=0;
+								tempUsedEventsList=[];
+								$mdDialog.hide();
+								$scope.loadWebhookListPaging();
+							}
+							else
+							{
+								notifications.toast("Webhook Creation Failed", "error");
+							}
+							//$scope.webhook={};
+							vm.webhookSubmitted = false;
+						}).error(function (data) {
+							$mdDialog.hide();
+							console.log(data);
+							vm.webhookSubmitted = false;
+						})
+					}
+					else
+					{
+						notifications.toast("Select Events for the Webhook", "error");
+						vm.webhookSubmitted = false;
+					}
 
-				  if(tempEventsSelected)
-				  {
-					  $charge.webhook().updateWH(webhookObj).success(function (data) {
-						  console.log(data);
-						  // debugger;
-						  if(data.error=="00000")
-						  {
-							  notifications.toast("Webhook Updated Successfully", "success");
-							  $scope.resetWebhook();
-							  $mdDialog.hide();
-						  }
-						  else
-						  {
-							  notifications.toast("Webhook Updating Failed", "error");
-						  }
-						  //$scope.webhook={};
-						  vm.webhookSubmitted = false;
-					  }).error(function (data) {
-						  $mdDialog.hide();
-						  console.log(data);
-						  vm.webhookSubmitted = false;
-					  })
-				  }
-				  else
-				  {
-					  notifications.toast("Select Events for the Webhook", "error");
-					  vm.webhookSubmitted = false;
-				  }
+				}
+			}
+			else
+			{
+				if (vm.webHooks.$valid == true) {
+					vm.webhookSubmitted = true;
 
-			  }
-		  }
-	  }
+					var webhookObj={};
+					var tempEventsSelected=false;
+					webhookObj.guWebhookId=$scope.webhook.guWebhookId;
+					webhookObj.endPoint=$scope.webhook.endPoint;
+					webhookObj.type=$scope.webhook.type;
+					webhookObj.createdDate=new Date();
+					webhookObj.isEnabled=true;
+					webhookObj.eventCodes=[];
 
-  }
+					for (var i = 0; i < vm.webhookEventList.length; i++) {
+						if(vm.webhookEventList[i].isSelected)
+						{
+							webhookObj.eventCodes.push(vm.webhookEventList[i].eventType);
+							tempEventsSelected=true;
+						}
+					}
+
+					if(tempEventsSelected)
+					{
+						$charge.webhook().updateWH(webhookObj).success(function (data) {
+							console.log(data);
+							// debugger;
+							if(data.error=="00000")
+							{
+								notifications.toast("Webhook Updated Successfully", "success");
+								$scope.resetWebhook();
+								$mdDialog.hide();
+							}
+							else
+							{
+								notifications.toast("Webhook Updating Failed", "error");
+							}
+							//$scope.webhook={};
+							vm.webhookSubmitted = false;
+						}).error(function (data) {
+							$mdDialog.hide();
+							console.log(data);
+							vm.webhookSubmitted = false;
+						})
+					}
+					else
+					{
+						notifications.toast("Select Events for the Webhook", "error");
+						vm.webhookSubmitted = false;
+					}
+
+				}
+			}
+		}
+
+	}
 })();
