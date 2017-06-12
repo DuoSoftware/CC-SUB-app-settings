d1ea60db3497ce19aac67c59d0962db69d8405a5
Ishara <gunathilakaishara@hotmail.com>
2017-06-12 16:24:48 +0530

    display menu button if only gateway support true

--- settings/settings.module.js
+++ settings/settings.module.js
@@ -1,8 +1,8 @@
 //////////////////////////////////////
 // App : Settings                   //
 // Owner : Suvethan                 //
-// Last changed date : 2017/06/02   //
-// Version : 6.1.0.15              //
+// Last changed date : 2017/06/12   //
+// Version : 6.1.0.16              //
 // Updated By : Ishara              //
 //////////////////////////////////////
 
