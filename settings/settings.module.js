58a5794e512b5d800db026f242b6eb819b0d6e5b
Kasun <kasun.w@duosoftware.com>
2017-10-11 11:09:47 +0530

    Version 6.1.0.30

--- settings/settings.module.js
+++ settings/settings.module.js
@@ -1,9 +1,9 @@
 //////////////////////////////////////
 // App : Settings                   //
 // Owner : Suvethan                 //
-// Last changed date : 2017/09/20   //
-// Version : 6.1.0.28               //
-// Updated By : Gihan               //
+// Last changed date : 2017/10/11   //
+// Version : 6.1.0.30               //
+// Updated By : Kasun               //
 //////////////////////////////////////
 
 (function ()
