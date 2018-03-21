373de43b240008a0866c68c9508c060a02a41f7e
gihan herath <gihan.h@hotmail.com>
2018-03-21 15:09:35 +0530

    integration tab validations

--- settings/settings.module.js
+++ settings/settings.module.js
@@ -1,7 +1,7 @@
 //////////////////////////////////////
 // App : Settings                   //
 // Owner : Suvethan                 //
-// Last changed date : 2018/03/13   //
+// Last changed date : 2018/03/21   //
 // Version : 6.1.0.38               //
 // Updated By : Gihan               //
 //////////////////////////////////////
