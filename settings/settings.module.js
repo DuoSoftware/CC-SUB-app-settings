0f02257d860b4d279220d83640f6cf8117d1898f
KasunW <kasun.w@duosoftware.com>
2018-04-17 16:11:29 +0530

    Version 6.1.0.41
    Unwanted BaseCurrency validation removed

--- settings/settings.module.js
+++ settings/settings.module.js
@@ -32,8 +32,8 @@
 					security: ['$q','mesentitlement','$timeout','$rootScope','$state','$location', function($q,mesentitlement,$timeout,$rootScope,$state, $location){
 						return $q(function(resolve, reject) {
 							$timeout(function() {
-								// if (true) {
-									if ($rootScope.isBaseSet2) {
+								if (true) {
+									// if ($rootScope.isBaseSet2) {
 									resolve(function () {
 										var entitledStatesReturn = mesentitlement.stateDepResolver('settings');
 
